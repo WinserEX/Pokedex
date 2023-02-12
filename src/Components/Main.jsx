638615acad9458266b38fc8be05d1a2a874b3504
WinserEX <winser500@gmail.com>
2023-02-12 00:23:04 -0400

    Added further styling, encountered a bug where Cards overlap each other when reaching lg breakpoint

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "./Card";
+import Pokeinfo from "./Pokeinfo";
 
 const Main = () => {
     return(
@@ -14,7 +15,7 @@ const Main = () => {
                     <Card />
                 </div>
                 <div className="right-content">
-                    
+                    <Pokeinfo />
                 </div>
            </div>
         </>
