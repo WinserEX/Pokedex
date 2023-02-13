8bba02a1b775ee3b7846af1ac8c77088a9a37e97
WinserEX <winser500@gmail.com>
2023-02-12 22:47:24 -0400

    Added several new functions in order to get data from API

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import Pokeinfo from "./Pokeinfo";
+import axios from "axios";
 
 const Main = () => {
+    const [pokeData, setPokedata] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
+    const [nextUrl, setNextUrl] = useState();
+    const [prevUrl, setPrevUrl] = useState();
+
+    const pokeFun = async() => {
+        setLoading(true)
+        const res = await axios.get(url);
+        setNextUrl(res.data.next);
+        setPrevUrl(res.data.previous);
+        getPokemon(res.data.results)
+        setLoading(false)
+        //console.log(pokeData)
+    }
+
+    const getPokemon = async(res) => {
+        res.map(async(item)=> {
+            const result = await axios.get(item.url)
+            //console.log(result.data);
+            setPokedata(state => {
+                state = [...state, result.data]
+                return state;
+            })
+        })
+    }
+    
+    useEffect(()=> {
+        pokeFun();
+    },[url])
+
     return(
         <>
            <div className="container">
